eeb24bc80c9fd265e6f8c6904507bf9bb7f6b780
Matthew Heun <matthew.heun@me.com>
2019-02-27 14:24:44 -0500

    Working on iea_df function.

--- R/initialize.R
+++ R/initialize.R
@@ -1,79 +1,64 @@
-iea_df <- function(iea_file){
-  conn <- file(iea_file, open="rt")
-  header <- conn %>% readLines(n = 2) #read in header
-  close(conn)
-  # Check whether the conversion has already happened.
-  if (header[[1]] %>% startsWith("COUNTRY,FLOW,PRODUCT,")) {
-    # slurp the file
-    IEAData_raw <- data.table::fread(iea_file, header = TRUE)
-  } else if (header[[1]] %>% startsWith(",,TIME,") & header[[2]] == "COUNTRY,FLOW,PRODUCT") {
+#' Load IEA data from csv file
+#'
+#' This function reads an IEA extended energy balances file and
+#' converts it to a data frame with appropriatly-labelled columns.
+#' Either \code{iea_file} or \code{text} can be specified, but not both.
+#' The first line of \code{iea_file} is expected to start with \code{expected_start_1st_line}, and
+#' the second line is expected to be \code{expected_2nd_line}.
+#' 
+#' This function is expected to work even as more years are added at the right of \code{iea_file}, 
+#' because column names are constructed from the first line of \code{iea_file} (which contains years).
+#' 
+#' The data frame returned from this function is not ready to be used in R, 
+#' because rows are not unique, and 
+#' several empty cells are filled with ".." or "x".
+#' Call \code{augment_iea_data()} to ready the data frame for use.
+#'
+#' @param iea_file a string containing the path to a .csv file of extended energy balances from the IEA.
+#' @param text a connection to \code{iea_file}.
+#' @param expected_1st_line_start the expected start of the first line of \code{iea_file}. Default is "\code{,,TIME}".
+#' @param expected_2nd_line the expected second line of \code{iea_file}. Default is "\code{COUNTRY,FLOW,PRODUCT}".
+#'
+#' @return a data frame containing the as-read IEA data file
+#' 
+#' @export
+iea_df <- function(iea_file = NULL, text = NULL, expected_1st_line_start = ",,TIME", expected_2nd_line = "COUNTRY,FLOW,PRODUCT"){
+  assertthat::assert_that(xor(is.null(iea_file), is.null(text)), msg = "need to supply one but not both of iea_file and text arguments to iea_df")
+  if (!is.null(iea_file)) {
+    conn <- file(iea_file, open="rt") # open file connection
   } else {
-    # Looks like this is not a good file.
+    # text has been provided
+    conn <- textConnection(text)
   }
-  # Slurp the file
-  IEAData_temp <- data.table::fread(iea_file, header = FALSE, sep = ",", verbose = TRUE)
-  # Create column names
-  cnames <- gsub(pattern = ",,TIME,", replacement = "COUNTRY,FLOW,PRODUCT,", header[[1]])
-  IEAData_raw <- IEAData_temp %>% 
-    magrittr::set_names(cnames %>% strsplit(",") %>% unlist())
-  
-  
-  
-  header <- 
-  headerlines <- strsplit(unfixed_header, split = newlinechar) %>% unlist()
-  nlines <- length(headerlines)
-  # Reject this input if it doesn't have 1 or 2 lines.
-  assertthat::assert_that(nlines %in% c(1,2), msg = paste("unfixed_header should have only 1 or 2 lines:", nlines, "lines were found."))
-  
-  # Check to see if this header is already "fixed"
-  if (nlines == 1 & unfixed_header %>% startsWith(correct_first_line_lead)) {
-    return(unfixed_header)
+  header <- conn %>% readLines(n = 2) # read in header
+  close(conn)
+  # Check whether header has the form we expect.
+  assertthat::assert_that(length(header) == 2, msg = "couldn't read 2 lines in iea_df")
+  assertthat::assert_that(header[[1]] %>% startsWith(expected_1st_line_start) & header[[2]] == expected_2nd_line, 
+                          msg = paste0("In iea_df, input data didn't start with '", expected_start_1st_line, "' or second line wasn't '", expected_2nd_line, "'")) 
+  if (!is.null(iea_file)) {
+    # Slurp the file. This slurping ignores the header, which fread deems to be the first 2 lines.
+    # Note that I'm using data.table::fread at the recommendation of
+    # https://statcompute.wordpress.com/2014/02/11/efficiency-of-importing-large-csv-files-in-r/
+    # which indicates this function is significantly faster than other options.
+    IEAData_noheader <- data.table::fread(file = iea_file, header = FALSE, sep = ",")
+  } else {
+    IEAData_noheader <- data.table::fread(text = text, header = FALSE, sep = ",")
   }
-  
-  if (nlines == 2 & headerlines[[1]] %>% startsWith(correct_first_line_lead) & headerlines[[2]] %>% startsWith(correct_first_line_lead)) {
-    # Return only the first line.
-    return(headerlines[[1]])
-  }
-  # Ensure that we have 2 lines
-  assertthat::assert_that(nlines == 2, msg = "length of unfixed_header != 2 in fix_header()")
-  out <- gsub(pattern = incorrect_first_line_lead, replacement = correct_first_line_lead, x = headerlines[[1]])
-  return(out)
-  
+  # At this point, the IEAData_noheader data frame has default (meaningless) column names, V1, V2, V3, ...  
+  # Create column names from the first two lines of the file. 
+  # The code here should be robust to adding more years through time,
+  # because it simply replaces the first 3 items with appropriate values from the 2nd line.
+  colnames <- gsub(pattern = expected_1st_line_start, replacement = paste0(expected_2nd_line), header[[1]]) %>% 
+    strsplit(",") %>% 
+    unlist()
+  IEAData_noheader %>% 
+    magrittr::set_names(colnames) %>% 
+    as.data.frame()
 }
 
 
 
-#' Fix IEA data header
-#' 
-#' The IEA extended energy balance files headers are not conducive to creating data frames.
-#' They contain two-line headers with blanks in the wrong places.
-#' This function fixes the header to ensure that it works properly.
-#'
-#' @param unfixed_header the header (first two lines) of an IEA data file as a string.
-#'
-#' @return a fixed version of the header as a string.
-#' 
-#' @export
-#'
-#' @examples
-#' fix_header(",,TIME,1960,1961\nCOUNTRY,FLOW,PRODUCT")
-fix_header <- function(unfixed_header, newlinechar = "\n", incorrect_first_line_lead = ",,TIME", correct_first_line_lead = "COUNTRY,FLOW,PRODUCT"){
-  headerlines <- strsplit(unfixed_header, split = newlinechar) %>% unlist()
-  nlines <- length(headerlines)
-  # Reject this input if it doesn't have 1 or 2 lines.
-  assertthat::assert_that(nlines %in% c(1,2), msg = paste("unfixed_header should have only 1 or 2 lines:", nlines, "lines were found."))
-
-  # Check to see if this header is already "fixed"
-  if (nlines == 1 & unfixed_header %>% startsWith(correct_first_line_lead)) {
-    return(unfixed_header)
-  }
+augment_iea_df <- function(.iea_df){
   
-  if (nlines == 2 & headerlines[[1]] %>% startsWith(correct_first_line_lead) & headerlines[[2]] %>% startsWith(correct_first_line_lead)) {
-    # Return only the first line.
-    return(headerlines[[1]])
-  }
-  # Ensure that we have 2 lines
-  assertthat::assert_that(nlines == 2, msg = "length of unfixed_header != 2 in fix_header()")
-  out <- gsub(pattern = incorrect_first_line_lead, replacement = correct_first_line_lead, x = headerlines[[1]])
-  return(out)
 }
--- tests/testthat/test-initialize.R
+++ tests/testthat/test-initialize.R
@@ -3,6 +3,14 @@ context("Initialize package")
 ###########################################################
 
 test_that("fix_header works", {
+  expect_error(iea_df(text = "abc"), "couldn't read 2 lines in iea_df")
+  expect_error(iea_df(text = "abc\n123"), "In iea_df, input data didn't start with ',,TIME,' or second line wasn't 'COUNTRY,FLOW,PRODUCT'")
+  
+  expect_equal(iea_df(text = ",,TIME,1960,1961\nCOUNTRY,FLOW,PRODUCT\nWorld,Production,Hard coal,42,43"), 
+               data.frame(COUNTRY = "World", FLOW = "Production", PRODUCT = "Hard coal", `1960` = 42, `1961` = 43))
+  
+  
+  
   iea_file <- file.path("~/Documents/Calvin stuff/Useful Work/IEA Data/Extended-Energy-Balances-2018/Extended-Energy-Balances-2018-full-ktoe.csv")
   
   
