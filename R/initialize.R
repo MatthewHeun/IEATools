a66cf1a03a29da6e082fe6237c4088424b5565ce
MatthewHeun <matthew.heun@me.com>
2019-02-27 22:08:41 -0500

    Working on initialize.

--- R/initialize.R
+++ R/initialize.R
@@ -1,31 +1,38 @@
 #' Load IEA data from csv file
 #'
 #' This function reads an IEA extended energy balances file and
-#' converts it to a data frame with appropriatly-labelled columns.
-#' Either \code{iea_file} or \code{text} can be specified, but not both.
-#' The first line of \code{iea_file} is expected to start with \code{expected_start_1st_line}, and
+#' converts it to a data frame with appropriately-labelled columns.
+#' One of \code{iea_file} or \code{text} must be specified, but not both.
+#' The first line of \code{iea_file} or \code{text} 
+#' is expected to start with \code{expected_start_1st_line}, and
 #' the second line is expected to be \code{expected_2nd_line}.
+#' If those conditions are not met, execution is halted.
 #' 
-#' This function is expected to work even as more years are added at the right of \code{iea_file}, 
+#' This function is expected to work even as more years are added
+#' as columns at the right of \code{iea_file}, 
 #' because column names are constructed from the first line of \code{iea_file} (which contains years).
 #' 
 #' The data frame returned from this function is not ready to be used in R, 
 #' because rows are not unique, and 
 #' several empty cells are filled with ".." or "x".
-#' Call \code{augment_iea_data()} to ready the data frame for use.
+#' Call \code{augment_iea_data()} to prepare the data frame for use.
 #'
-#' @param iea_file a string containing the path to a .csv file of extended energy balances from the IEA.
-#' @param text a connection to \code{iea_file}.
+#' @param iea_file a string containing the path to a .csv file of extended energy balances from the IEA
+#' @param text a character string that can be parsed as IEA extended energy balances
 #' @param expected_1st_line_start the expected start of the first line of \code{iea_file}. Default is "\code{,,TIME}".
 #' @param expected_2nd_line the expected second line of \code{iea_file}. Default is "\code{COUNTRY,FLOW,PRODUCT}".
 #'
-#' @return a data frame containing the as-read IEA data file
+#' @return a data frame containing the as-read IEA data
 #' 
 #' @export
+#' 
+#' @examples 
+#' iea_df(text = ",,TIME,1960,1961\nCOUNTRY,FLOW,PRODUCT\nWorld,Production,Hard coal,42,43")
 iea_df <- function(iea_file = NULL, text = NULL, expected_1st_line_start = ",,TIME", expected_2nd_line = "COUNTRY,FLOW,PRODUCT"){
-  assertthat::assert_that(xor(is.null(iea_file), is.null(text)), msg = "need to supply one but not both of iea_file and text arguments to iea_df")
+  assertthat::assert_that(xor(is.null(iea_file), is.null(text)), 
+                          msg = "need to supply one but not both of iea_file and text arguments to iea_df")
   if (!is.null(iea_file)) {
-    conn <- file(iea_file, open="rt") # open file connection
+    conn <- file(iea_file, open = "rt") # open file connection
   } else {
     # text has been provided
     conn <- textConnection(text)
@@ -48,7 +55,8 @@ iea_df <- function(iea_file = NULL, text = NULL, expected_1st_line_start = ",,TI
   # At this point, the IEAData_noheader data frame has default (meaningless) column names, V1, V2, V3, ...  
   # Create column names from the first two lines of the file. 
   # The code here should be robust to adding more years through time,
-  # because it simply replaces the first 3 items with appropriate values from the 2nd line.
+  # because it simply replaces the first 3 items of the first line 
+  # with appropriate values from the 2nd line.
   colnames <- gsub(pattern = expected_1st_line_start, replacement = paste0(expected_2nd_line), header[[1]]) %>% 
     strsplit(",") %>% 
     unlist()
--- tests/testthat/test-initialize.R
+++ tests/testthat/test-initialize.R
@@ -2,25 +2,18 @@
 context("Initialize package")
 ###########################################################
 
-test_that("fix_header works", {
+test_that("iea_df works", {
+  # Test with only 1 line
   expect_error(iea_df(text = "abc"), "couldn't read 2 lines in iea_df")
+  # Test with 2 lines but of wrong style.
   expect_error(iea_df(text = "abc\n123"), "In iea_df, input data didn't start with ',,TIME' or second line wasn't 'COUNTRY,FLOW,PRODUCT'")
-  
+  # Test with text that is expected to parse correctly.
   expect_equal(iea_df(text = ",,TIME,1960,1961\nCOUNTRY,FLOW,PRODUCT\nWorld,Production,Hard coal,42,43"), 
-               data.frame(COUNTRY = "World", FLOW = "Production", PRODUCT = "Hard coal", `1960` = 42, `1961` = 43))
-  
-  
-  
+               data.frame(COUNTRY = "World", FLOW = "Production", PRODUCT = "Hard coal", `1960` = 42, `1961` = 43, 
+                          check.names = FALSE, stringsAsFactors = FALSE))
+})
+
+test_that("augment_iea_df works", {
   iea_file <- file.path("~/Documents/Calvin stuff/Useful Work/IEA Data/Extended-Energy-Balances-2018/Extended-Energy-Balances-2018-full-ktoe.csv")
   
-  
-  
-  expect_error(fix_header("abc\n123\nxyz"), "unfixed_header should have only 1 or 2 lines: 3 lines were found")
-  expect_error(fix_header("abc"))
-
-  # Note: Need to build the package before this file is put in the correct location.
-  mock_filename <- system.file("extdata", "example-unfixed-header.csv", package = "IEAData")
-  mock_file <- readChar(mock_filename, file.info(mock_filename)$size)
-  expect_true(mock_file != "")
-  expect_true(fix_header(mock_file) %>% startsWith("COUNTRY,FLOW,PRODUCT,1960"))
 })
