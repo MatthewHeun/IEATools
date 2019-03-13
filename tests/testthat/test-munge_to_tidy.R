b4fc5a0a6abe9fb1af9086b2e5a6c5751bbc2770
MatthewHeun <matthew.heun@me.com>
2019-03-12 22:45:58 -0400

    Better variable name agg_rows --> n_agg_rows

--- tests/testthat/test-munge_to_tidy.R
+++ tests/testthat/test-munge_to_tidy.R
@@ -11,7 +11,7 @@ test_that("remove_agg_memo_flows works as expected", {
     rename_iea_df_cols() %>% 
     remove_agg_memo_flows()
   # Verify that none of the aggregation flows are present
-  agg_rows <- cleaned %>% 
+  n_agg_rows <- cleaned %>% 
     dplyr::filter(Flow == "Total primary energy supply" |
                     Flow == "Total final consumption" | 
                     Flow == "Transformation processes" |
@@ -21,7 +21,7 @@ test_that("remove_agg_memo_flows works as expected", {
                     Flow == "Other" |
                     Flow == "Non-energy use") %>% 
     nrow()
-  expect_equal(agg_rows, 0)
+  expect_equal(n_agg_rows, 0)
 })
 
 test_that("munge_aug_iea_to_tidy works as expected", {
