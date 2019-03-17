e0abad3de3e85e14cfed3132e27a1f66bd2b2c89
MatthewHeun <matthew.heun@me.com>
2019-03-17 17:21:44 -0400

    Added description for constants.

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Coal and coal products
+#'
+#' A string vector containing names of products classified by the IEA as coal and coal products.
+#' 
+#' @format A string vector with 16 entries
+"coal_and_coal_products"
+
+
+#' Oil and oil products
+#'
+#' A string vector containing names of products classified by the IEA as oil and oil products.
+#' 
+#' @format A string vector with 23 entries
+"oil_and_oil_products"
+
+
+#' Renewable products
+#'
+#' A string vector containing names of products classified by the IEA as renewables.
+#' 
+#' @format A string vector with 23 entries
+"renewable_products"
+
+
+#' Biofuel and waste products
+#'
+#' A string vector containing names of products classified by the IEA as biofuel and waste products.
+#' 
+#' @format A string vector with 10 entries
+"biofuel_and_waste_products"
--- R/munge_to_tidy.R
+++ R/munge_to_tidy.R
@@ -357,7 +357,7 @@ augment_iea_df <- function(.iea_df,
 #' If the suffix of the value of the `Flow` column is already "`(Product)`", 
 #' no changes to the value in the `Flow` column will be made.
 #'
-#' @param .iea_df 
+#' @param .iea_df a IEA data frame whose columns have been renamed by [rename_iea_df_cols()]
 #' @param flow the name of the flow column in `.iea_df`.  Default is "`Flow`".
 #' @param production a string that identifies production in the flow column. Default is "`Production`".
 #' @param product the name of the product column in `.iea_df`.  Default is "`Product`". 
@@ -370,7 +370,7 @@ augment_iea_df <- function(.iea_df,
 add_production_details <- function(.iea_df, flow = "Flow", production = "Production", product = "Product"){
   .iea_df %>% 
     dplyr::mutate(
-      !!as.name(flow) := case_when(
+      !!as.name(flow) := dplyr::case_when(
         !!as.name(flow) == production & !endsWith(!!as.name(flow), paste0("(", !!as.name(product), ")")) 
           ~ paste0(!!as.name(flow), " (", !!as.name(product), ")"), 
         TRUE ~ !!as.name(flow)
