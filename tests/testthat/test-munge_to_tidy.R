1658cd78ad22210a549fde59b9b5a35f105b34cb
MatthewHeun <matthew.heun@me.com>
2019-03-17 18:52:29 -0400

    Verify that add_production_details 2x does no harm

--- tests/testthat/test-munge_to_tidy.R
+++ tests/testthat/test-munge_to_tidy.R
@@ -119,7 +119,7 @@ test_that("tidy_iea works as expected", {
 })
 
 test_that("add_production_details works as expected", {
-  prod_details <- file.path("extdata", "GH-ZA-ktoe-Extended-Energy-Balances-sample.csv") %>% 
+  Prod_details <- file.path("extdata", "GH-ZA-ktoe-Extended-Energy-Balances-sample.csv") %>% 
     system.file(package = "IEATools") %>% 
     iea_df() %>% 
     rename_iea_df_cols() %>% 
@@ -127,8 +127,18 @@ test_that("add_production_details works as expected", {
     remove_agg_memo_flows() %>% 
     tidy_iea_df() %>% 
     add_production_details()
-  expect_false(any(prod_details$Flow == "Production"))
-  Production <- prod_details %>% filter(startsWith(Flow, "Production"))
+  expect_false(any(Prod_details$Flow == "Production"))
+  Production <- Prod_details %>% filter(startsWith(Flow, "Production"))
   expect_true(all(Production$Flow %>% startsWith("Production (")))
   expect_true(all(Production$Flow %>% endsWith(")")))
+  # add_production_details should do nothing if it has already been done.
+  Prod_details_2 <- Prod_details %>% 
+    add_production_details()
+  # If the function applied the details twice, there will be some Flows
+  # that contain the string ") (".
+  # So search for that.
+  expect_false(any(Prod_details_2 %>% 
+                     filter(startsWith(Flow, "Production")) %>% 
+                     extract2("Flow") %>% 
+                     grepl(pattern = "\\) \\(")))
 })
