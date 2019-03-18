8758a07fcd21475d9fb649b8deae0b36cacf437c
MatthewHeun <matthew.heun@me.com>
2019-03-17 21:52:02 -0400

    Added a script that loads AllIEAData.

---
+++ data-raw/load_everything.R
@@ -0,0 +1,2 @@
+# The next line loads all IEA data from the location on my disk.
+AllIEAData <- load_tidy_iea_df("~/Documents/Calvin stuff/Useful Work/IEA Data/Extended-Energy-Balances-2018/Extended-Energy-Balances-2018-full-ktoe.csv")
