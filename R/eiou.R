eead14651c61af5aa73264688e5467c15878ac85
MatthewHeun <matthew.heun@me.com>
2019-03-18 22:59:03 -0400

    Better specify_coal_production function.

--- R/eiou.R
+++ R/eiou.R
@@ -1,6 +1,6 @@
 #' Specify production industries
 #' 
-#' The IEA extended energy balance data include 
+#' The IEA extended energy balances include 
 #' some `Flow`s that identify `Energy industry own use` (EIOU), 
 #' the consumption of energy by energy-producing industries.
 #' But some industries that receive EIOU do not produce anything.
@@ -11,40 +11,52 @@
 #' replacing the generic `Production` industry with 
 #' specific industries.
 #' 
-#' In particular, the following changes are made:
+#' The following changes are made to `.tidy_iea_df`:
 #' 
 #' * The `Production` industry for `coal_and_coal_products` 
 #'   is replaced by `Coal mines`.
-#' * A `Resources (coal)` industry is created.
+#' * A `Resources (coal)` industry is created which becomes the 
+#'   originator of `coal_and_coal_products` in the energy conversion chain.
+#' * Flows from `Resources (Coal)` to `Coal mines` are added.
+#' * The `Coal mines (energy)` `Flow` is replaced by `Coal mines`.
 #'
-#' @param .tidy_iea_data 
-#' @param flow 
-#' @param product 
-#' @param production 
-#' @param coal_mines 
-#' @param coals 
+#' @param .tidy_iea_df an IEA data frame whose columns have been renamed by [rename_iea_df_cols()]
+#' @param flow_aggregation_point the name of the flow aggregation point column in `.tidy_iea_df`. Default is "`Flow.aggregation.point`".
+#' @param flow the name of the flow column in `.tidy_iea_df`.  Default is "`Flow`".
+#' @param production a string identifying production in the flow column. Default is "`Production`".
+#' @param transformation_processes a string identifying transformation processes in the flow column of `.tidy_iea_df`. Default is "`Transformation processes`".
+#' @param product the name of the product column in `.tidy_iea_df`.  Default is "`Product`".
+#' @param coal_mines a string identifying coal mines in the flow column. Default is "`Coal mines`".
+#' @param coal_mines_eiou a string identifying energy industry by coal mines. Default is "`coal_mines` + `(energy)`".
+#' @param coals a vector of strings identifying coal and coal products. Default is `coal_and_coal_products`.
+#' @param e_dot the name of the energy column in `.tidy_eia_df`. Default is "`E.dot`".
 #'
-#' @return
+#' @return `.tidy_iea_df` with adjusted coal and coal mine information
 #' 
 #' @export
 #'
 #' @examples
-specify_coal_production <- function(.tidy_iea_data,
+#' load_tidy_iea_df() %>% 
+#'   specify_coal_production() %>% 
+#'   add_psut_matnames() %>% 
+#'   filter(Flow %in% c("Resources (Coal)", "Coal mines")) %>%
+#'   select(-Method, -Last.stage, -Ledger.side, -Unit)
+specify_coal_production <- function(.tidy_iea_df,
                                flow_aggregation_point = "Flow.aggregation.point",
-                               transformation_processes = "Transformation processes",
                                flow = "Flow", 
-                               product = "Product",
                                production = "Production", 
+                               transformation_processes = "Transformation processes",
+                               product = "Product",
                                coal_mines = "Coal mines",
                                coal_mines_eiou = paste(coal_mines, "(energy)"),
                                coals = coal_and_coal_products,
                                resources_coal = "Resources (Coal)",
                                e_dot = "E.dot"){
   # Find rows of EIOU by Coal mines
-  CoalMine_EIOU <- .tidy_iea_data %>% 
+  CoalMine_EIOU <- .tidy_iea_df %>% 
     filter(!!as.name(flow) == coal_mines_eiou)
   # Find rows of Production of coals 
-  Coal_production <- .tidy_iea_data %>% 
+  Coal_production <- .tidy_iea_df %>% 
     dplyr::filter(!!as.name(flow) == production & !!as.name(product) %in% coals)
   # Make resource rows for these rows.
   Coal_resources <- Coal_production %>% 
@@ -54,7 +66,7 @@ specify_coal_production <- function(.tidy_iea_data,
   if (nrow(CoalMine_EIOU) == 0) {
     # We have no EIOU for coal mines.
     # We need only to change Production to Resources (Coal)
-    .tidy_iea_data %>% 
+    .tidy_iea_df %>% 
       # Replace Production with Resources (Coal)
       dplyr::mutate(
         !!as.name(flow) := case_when(
@@ -79,7 +91,7 @@ specify_coal_production <- function(.tidy_iea_data,
     )
 
   # Bundle everything and return
-  .tidy_iea_data %>% 
+  .tidy_iea_df %>% 
     # Replace the "Coal mines (energy)" Flow in EIOU with "Coal mines"
     dplyr::mutate(
       !!as.name(flow) := case_when(
@@ -87,7 +99,7 @@ specify_coal_production <- function(.tidy_iea_data,
       )
     ) %>% 
     # Delete the original Coal production rows
-    dplyr::anti_join(Coal_production, by = names(.tidy_iea_data)) %>% 
+    dplyr::anti_join(Coal_production, by = names(.tidy_iea_df)) %>% 
     # And bind the replacements.
     dplyr::bind_rows(Coal_resources, Mine_input, Mine_output)
 }
