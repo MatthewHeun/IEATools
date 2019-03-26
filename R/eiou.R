cd53704a5659b38e9d99117d57146e56a335c0e3
MatthewHeun <matthew.heun@me.com>
2019-03-25 22:17:12 -0400

    Added functionality to specify_eiou_tp.

--- R/eiou.R
+++ R/eiou.R
@@ -1,25 +1,52 @@
-#' Specify destinations for inter-industry energy industry own use flows
+#' Specify destinations for energy industry own use flows into transformation processes 
+#' 
+#' The extended energy balance data from the IEA includes 
+#' Energy industry own use (EIOU) for many transformation processes.
+#' Unfortunately, the EIOU flows into industries that aren't included in transformation processes.
+#' For example, `Electricity` is consumed by 
+#' `Own use in electricity, CHP and heat plants`, 
+#' which is not transformation process.
+#' We have to make some decisions to ensure that 
+#' EIOU is routed to actual transformation processes.
+#' See details for a list of actual changes made to the `.tidy_iea_df` data frame.
+#' 
+#' The following changes are made to the `.tidy_iea_df` data frame:
+#' 1. EIOU classified as `own_use_elect_chp_heat` is sent to `main_act_producer_elect`.
+#' 2. EIOU classified as `pumped_storage` is sent to `main_act_producer_elect`.
+#' 3. EIOU classified as `nuclear_industry` is sent to `main_act_producer_elect`.
+#' 4. EIOU classified as `non_spec_energy` is sent to `nonspecenergy_reclassify`.
 #'
-#' @param .tidy_iea_df 
+#' @param .tidy_iea_df an IEA data frame whose columns have been renamed by [rename_iea_df_cols()]
 #' @param flow 
 #' @param own_use_elect_chp_heat 
+#' @param flow_aggregation_point 
+#' @param eiou 
+#' @param pumped_storage 
+#' @param nuclear_industry 
+#' @param non_spec_energy 
+#' @param nonspecenergy_reclassify 
 #' @param main_act_producer_elect 
-#' @param product 
-#' @param electricity 
 #'
-#' @return
+#' @return a modified version of `.tidy_iea_df`
+#' 
 #' @export
 #'
 #' @examples
-specify_eiou <- function(.tidy_iea_df,
-                         flow_aggregation_point = "Flow.aggregation.point",
-                         eiou = "Energy industry own use",
-                         flow = "Flow", 
-                         own_use_elect_chp_heat = "Own use in electricity, CHP and heat plants",
-                         pumped_storage = "Pumped storage plants",
-                         main_act_producer_elect = "Main activity producer electricity plants",
-                         product = "Product",
-                         electricity = "Electricity"){
+#' library(dplyr)
+#' load_tidy_iea_df() %>% 
+#'   specify_eiou_tp() %>% 
+#'   filter(Flow.aggregation.point == "Energy industry own use" & 
+#'            Flow == "Main activity producer electricity plants")
+specify_eiou_tp <- function(.tidy_iea_df,
+                            flow_aggregation_point = "Flow.aggregation.point",
+                            eiou = "Energy industry own use",
+                            flow = "Flow", 
+                            own_use_elect_chp_heat = "Own use in electricity, CHP and heat plants",
+                            pumped_storage = "Pumped storage plants",
+                            nuclear_industry = "Nuclear industry",
+                            main_act_producer_elect = "Main activity producer electricity plants",
+                            non_spec_energy = "Non-specified (energy)",
+                            nonspecenergy_reclassify = "Oil and gas extraction"){
   .tidy_iea_df %>% 
     dplyr::mutate(
       !!as.name(flow) := dplyr::case_when(
@@ -31,7 +58,7 @@ specify_eiou <- function(.tidy_iea_df,
         # electricity production than CHP or heat production in each country, 
         # we apply "Own use in electricity, CHP and heat plants" to
         # "Main activity producer electricity plants".
-        !!as.name(flow) == own_use_elect_chp_heat & !!as.name(product) == electricity ~ main_act_producer_elect,
+        !!as.name(flow) == own_use_elect_chp_heat & !!as.name(flow_aggregation_point) == eiou ~ main_act_producer_elect,
         
         # When pumped storage is in the mix, 
         # the IEA data helpfully indicates EIOU assigned to "Pumped storage plants".
@@ -41,54 +68,22 @@ specify_eiou <- function(.tidy_iea_df,
         # apply EIOU by Pumped storage plants to 
         # the Industry in which production from Pumped storage plants is accounted:
         # Main activity producer electricity plants.
-        !!as.name(flow) == "Pumped storage plants" & !!as.name(flow_aggregation_point) == eiou ~ main_act_producer_elect,
-         
+        !!as.name(flow) == pumped_storage & !!as.name(flow_aggregation_point) == eiou ~ main_act_producer_elect,
+        
+        # If Nuclear is used, we need to reclassify EIOU by Nuclear plants
+        # to Main activity producer electricity plants.
+        !!as.name(flow) == nuclear_industry & !!as.name(flow_aggregation_point) == eiou ~ main_act_producer_elect,
+        
+        # Non-specified (energy) is an Industry that receives EIOU.
+        # However, Non-specified (energy) is not an Industry that makes anything.
+        # So, we need to reassign these EIOU flows somwehere.
+        # For the UK, the numbers for "Non-sepcified (energy)" are rather small.
+        # In the absence of any better information, we apply 
+        # "Non-specified (energy)" to nonspecenergy_reclassify.  
+        !!as.name(flow) == non_spec_energy & !!as.name(flow_aggregation_point) == eiou ~ nonspecenergy_reclassify,
+        
+        # Otherwise, just keep the same value for the flow column.
         TRUE ~ !!as.name(flow)
       )
     )
-  # Flow = case_when(
-  #   
-  #   # If Nuclear is used, we need to reclassify EIOU by Nuclear plants
-  #   # to Main activity producer electricity plants.
-  #   .$Flow == "Nuclear industry" & 
-  #     .$Flow.aggregation.point == "Energy industry own use" ~ "Main activity producer electricity plants",
-  #   
-  #   # Non-specified (energy) is an Industry that receives EIOU.
-  #   # However, Non-specified (energy) is not an Industry that makes anything.
-  #   # So, we need to reassign these EIOU flows somwehere.
-  #   # For the UK, the numbers for "Non-sepcified (energy)" are rather small.
-  #   # In the absence of any better information, we apply 
-  #   # "Non-specified (energy)" to nonspecenergy_reclassify.
-  #   .$Flow == "Non-specified (energy)" &
-  #     .$Flow.aggregation.point == "Energy industry own use" ~ nonspecenergy_reclassify,
-  #   
-  #   # Apply Production of coal and coal products to "Coal mines".
-  #   # This solves the problem in South Africa where "Coal mines" consume Electricity
-  #   # but coal Production is listed under "Production".
-  #   # This scenario causes a zero entry for "Coal mines" in the g vector and 
-  #   # subsequent inability to invert the g_hat matrix.
-  #   # The solution is to move Production of primary coal Products to "Coal mines" and out of "Production".
-  #   .$Flow == "Production" & .$Product %in% coal_and_coal_products ~ "Coal mines",
-  #   
-  #   # Apply Production of oil and oil products to "Oil and gas extraction".
-  #   # This solves the problem, first observed for GB in 1961
-  #   # where "Oil and gas extraction" consumes Natural gas (as EIOU)
-  #   # but Natural gas Production is listed under "Production".
-  #   # This scenario causes a zero entry for "Oil and gas extraction" in the U matrix and 
-  #   # subsequent inability to correctly swim upstream.
-  #   # The solution is to move Production of oil and oil Products to "Oil and gas extraction" and out of "Production".
-  #   .$Flow == "Production" & .$Product %in% c(oil_and_oil_products, "Natural gas") ~ "Oil and gas extraction",
-  #   
-  #   # When Flow is any Industry involved in TPES, we need to be more specific.
-  #   # If we don't separate these Flows, we run into trouble with
-  #   # upstream swims (all Products are produced even if only one is needed) and
-  #   # embodied energy calculations (many types of energy are embodied,
-  #   # even if only one sould be)
-  #   # .$Flow %in% p_machines ~ paste(Flow, Product, sep = " - "),
-  #   .$Flow %in% generic_p_machines ~ paste(Flow, Product, sep = " - "),
-  #   
-  #   # Otherwise, simply keep the existing value for Flow.
-  #   TRUE ~ .$Flow
-  # ) 
-  
 }
--- tests/testthat/test-eiou.R
+++ tests/testthat/test-eiou.R
@@ -7,10 +7,10 @@ context("Specify EIOU")
 test_that("specify_eiou works as expected for Own use in electricity, CHP and heat plants", {
   # Make a bogus data frame
   EIOU <- data.frame(Country = c("US", "US"), 
-                     Flow.aggregation.point = c("Nothing", "Nothing"),
+                     Flow.aggregation.point = c("Energy industry own use", "nothing"),
                      Flow = c("Own use in electricity, CHP and heat plants", "Own use in electricity, CHP and heat plants"), 
-                     Product = c("Electricity", "Nothing"), stringsAsFactors = FALSE)
-  EIOU_fixed <- specify_eiou(EIOU)
+                     stringsAsFactors = FALSE)
+  EIOU_fixed <- specify_eiou_tp(EIOU)
   # The first row is expected to change, because its Product is "Electricity"
   expect_equal(EIOU_fixed$Flow[[1]], "Main activity producer electricity plants")
   # The second row will not change, because its Product is "Nothing"
@@ -21,13 +21,37 @@ test_that("specify_eiou works as expected for pumped storage plants", {
   # Make a bogus data frame
   EIOU <- data.frame(Country = c("US", "US"), 
                      Flow.aggregation.point = c("Energy industry own use", "Nothing"),
-                     Product = c("Electricity", "Electricity"),
                      Flow = c("Pumped storage plants", "Pumped storage plants"), 
                      stringsAsFactors = FALSE)
-  EIOU_fixed <- specify_eiou(EIOU)
+  EIOU_fixed <- specify_eiou_tp(EIOU)
   # The first row is expected to change, because its Product is "Electricity"
   expect_equal(EIOU_fixed$Flow[[1]], "Main activity producer electricity plants")
   # The second row will not change, because its Product is "Nothing"
   expect_equal(EIOU_fixed$Flow[[2]], "Pumped storage plants")
 })
 
+test_that("specify_eiou works as expected for nuclear industry", {
+  # Make a bogus data frame
+  EIOU <- data.frame(Country = c("US", "US"), 
+                     Flow.aggregation.point = c("Energy industry own use", "Nothing"),
+                     Flow = c("Nuclear industry", "Nuclear industry"), 
+                     stringsAsFactors = FALSE)
+  EIOU_fixed <- specify_eiou_tp(EIOU)
+  # The first row is expected to change, because its Product is "Electricity"
+  expect_equal(EIOU_fixed$Flow[[1]], "Main activity producer electricity plants")
+  # The second row will not change, because its Product is "Nothing"
+  expect_equal(EIOU_fixed$Flow[[2]], "Nuclear industry")
+})
+
+test_that("specify_eiou works as expected for non-specified (energy)", {
+  # Make a bogus data frame
+  EIOU <- data.frame(Country = c("US", "US"), 
+                     Flow.aggregation.point = c("Energy industry own use", "Nothing"),
+                     Flow = c("Non-specified (energy)", "Non-specified (energy)"), 
+                     stringsAsFactors = FALSE)
+  EIOU_fixed <- specify_eiou_tp(EIOU)
+  # The first row is expected to change, because its Product is "Electricity"
+  expect_equal(EIOU_fixed$Flow[[1]], "Oil and gas extraction")
+  # The second row will not change, because its Product is "Nothing"
+  expect_equal(EIOU_fixed$Flow[[2]], "Non-specified (energy)")
+})
