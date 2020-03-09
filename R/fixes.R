21bb130a19c0ded0483074de456e1066547542b5
Matthew Heun <matthew.heun@me.com>
2020-03-09 19:57:57 -0400

    Writing functions to "fix" some time series

---
+++ R/fixes.R
@@ -0,0 +1,22 @@
+# 
+# This file contains functions to "fix" specifics about IEA extended energy balance data for some countries.
+# These functions should use data obtained from specific and detailed country studies.
+#
+
+fix_GHA_psb <- function(.iea_df) {
+  
+}
+
+
+fix_GHA_industry_electricity <- function(.iea_df) {
+  
+}
+
+
+
+fix_HND_fuels <- function(.iea_df) {
+  
+}
+
+
+
