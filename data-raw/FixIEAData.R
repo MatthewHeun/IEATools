40460b1a3d31580c437cf4f91e925f089cda1353
MatthewHeun <matthew.heun@me.com>
2020-03-15 16:17:04 -0400

    Now creating Fixed IEA industry electricity object.

--- data-raw/FixIEAData.R
+++ data-raw/FixIEAData.R
@@ -11,7 +11,8 @@ library(IEATools)
 library(magrittr)
 library(tidyselect)
 
-
+year_pattern <- "^-?\\d+$"
+  
 # Ghana's Primary solid biofuels data show a very large and dramatic decline from 1999 to 2000.
 # This decline is due to new survey data being used for the 2000 data.  
 # When we look at the PSB data on a per-capita basis, it is clear that 
@@ -24,19 +25,26 @@ library(tidyselect)
 # We read the data here and gather it.
 # Then, we make it available for use internally to the package.
 # The function fix_GHA_psb() makes use of these data.
-# FixedGHA_PSB <- read.delim(file = file.path("data-raw", "FixedGHPSB.tsv"), 
-#                            check.names = FALSE, stringsAsFactors = FALSE) %>% 
 Fixed_GHA_PSB <- openxlsx::read.xlsx(xlsxFile = file.path("data-raw", "GHA-PSB.xlsx"), sheet = "FixedGHPSB") %>% 
-  tidyr::pivot_longer(tidyselect::matches("^-?\\d+$"), names_to = "Year", values_to = "E.dot", ) %>% 
+  tidyr::pivot_longer(cols = tidyselect::matches(year_pattern), names_to = iea_cols$year, values_to = iea_cols$e_dot) %>% 
   dplyr::filter(
     # Eliminate rows that have 0 energy.
-    E.dot != 0
-  ) %>%
+    !!as.name(iea_cols$e_dot) != 0
+  ) %>% 
   dplyr::mutate(
-    Year = as.numeric(Year)
+    !!as.name(iea_cols$year) := as.numeric(!!as.name(iea_cols$year))
   )
 
-
+Fixed_GHA_Industry_Electricity <- openxlsx::read.xlsx(xlsxFile = file.path("data-raw", "GHA-IndustryElectricity.xlsx"), 
+                                                                           sheet = "FixedGHAIndustryElectricity") %>% 
+  tidyr::pivot_longer(tidyselect::matches(year_pattern), names_to = iea_cols$year, values_to = iea_cols$e_dot) %>% 
+  dplyr::filter(
+    # Eliminate rows that have 0 energy.
+    !!as.name(iea_cols$e_dot) != 0
+  ) %>% 
+  dplyr::mutate(
+    !!as.name(iea_cols$year) := as.numeric(!!as.name(iea_cols$year))
+  )
 
 
 
@@ -45,7 +53,7 @@ Fixed_GHA_PSB <- openxlsx::read.xlsx(xlsxFile = file.path("data-raw", "GHA-PSB.x
 # Use these data frames as internal objects in the package.
 # These objects are stored in R/sysdata.rda and can be accessed by
 # IEATools:::Fixed_GHA_PSB, for example.
-usethis::use_data(Fixed_GHA_PSB, internal = TRUE, overwrite = TRUE)
+usethis::use_data(Fixed_GHA_PSB, Fixed_GHA_Industry_Electricity, internal = TRUE, overwrite = TRUE)
 
 
 
