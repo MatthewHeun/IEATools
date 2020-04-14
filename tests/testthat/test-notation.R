541bc498b0581ec94c42d9f98d8d6646e5a28d14
Matthew Heun <matthew.heun@me.com>
2020-04-14 19:08:09 -0400

    Another test for paren_to_arrow

--- tests/testthat/test-notation.R
+++ tests/testthat/test-notation.R
@@ -6,6 +6,11 @@ test_that("arrow_to_paren works as expected", {
   expect_equal(arrow_to_paren("a -> b"), "b [a]")
   expect_equal(arrow_to_paren(c("a -> b", "c -> d")), c("b [a]", "d [c]"))
   
+})
+
+
+test_that("paren_to_arrow works as expected", {
   expect_equal(paren_to_arrow("b [a]"), "a -> b")
+  expect_equal(paren_to_arrow(c("b [a]", "d [c]")), c("a -> b", "c -> d"))
 })
 
