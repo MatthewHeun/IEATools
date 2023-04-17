a1bf33c8a698f2833241827d6002d97a0b38bbfd
Matthew Heun <matthew.heun@me.com>
2023-04-17 08:52:48 -0400

    fidy --> tidy

--- tests/testthat/test-templates.R
+++ tests/testthat/test-templates.R
@@ -242,7 +242,7 @@ test_that("eta_fu_template() works with tidy fu allocation data for 2021", {
 })
 
 
-test_that("eta_fu_template() works with fidy data from the default year", {
+test_that("eta_fu_template() works with tidy data from the default year", {
   tidy_specified_iea_data <- load_tidy_iea_df(apply_fixes = FALSE) %>% 
     specify_all()
   Eta_fu_template <- load_fu_allocation_data(sample_fu_allocation_table_path()) %>% 
