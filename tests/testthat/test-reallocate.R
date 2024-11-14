3ac0fd7a32db848ab2e8e9dfe368958c72873b39
MatthewHeun <matthew.heun@me.com>
2024-11-13 20:12:23 -0500

    Remove reallocation of Industry not elsewhere specified

--- tests/testthat/test-reallocate.R
+++ tests/testthat/test-reallocate.R
@@ -1,17 +1,3 @@
-test_that("reallocate_industry_nes() works as expected", {
-  psut <- load_tidy_iea_df() |> 
-    specify_all() |>  
-    prep_psut() |> 
-    dplyr::slice_head(n = 1)
-  
-  psut_reallocated <- psut |> 
-    reallocate_industry_nes()
-  
-  expect_true(!is.null(psut_reallocated))
-  
-  
-})
-
 
 test_that("reallocate_statistical_differences() works as expected", {
   psut <- load_tidy_iea_df() |> 
