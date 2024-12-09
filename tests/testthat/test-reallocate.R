892bb3866726b2d34a92c2253a217eec6ecfe67a
Matthew Heun <matthew.heun@me.com>
2024-12-09 09:31:41 -0500

    Re-add test-reallocate.R

---
+++ tests/testthat/test-reallocate.R
@@ -0,0 +1,9 @@
+test_that("reallocate_statistical_differences() works as expected", { 
+  psut <- load_tidy_iea_df() |> 
+    specify_all() |>  
+    prep_psut() |> 
+    dplyr::slice_head(n = 1)
+  psut_reallocated <- psut |> 
+    reallocate_statistical_differences()
+  expect_true(!is.null(psut_reallocated))
+})
