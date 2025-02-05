9dd2c7160723c27377bc6a0e365b993808389622
Matthew Heun <matthew.heun@me.com>
2025-02-05 14:36:20 -0500

    Working on reallocating statdiffs

--- R/reallocate.R
+++ R/reallocate.R
@@ -42,66 +42,119 @@
 reallocate_statistical_differences <- function(.sutmats, 
                                                stat_diffs = IEATools::tfc_compare_flows$statistical_differences, 
                                                R = IEATools::psut_cols$R, 
-                                               U_feed = IEATools::psut_cols$U_feed,
                                                U = IEATools::psut_cols$U,
+                                               U_feed = IEATools::psut_cols$U_feed,
+                                               U_eiou = IEATools::psut_cols$U_eiou,
+                                               r_eiou = IEATools::psut_cols$r_eiou,
                                                V = IEATools::psut_cols$V,
                                                Y = IEATools::psut_cols$Y, 
-                                               prime_suffix = "_prime",
-                                               R_prime = paste0(R, prime_suffix),
-                                               U_feed_prime = paste0(U_feed, prime_suffix),
-                                               U_prime = paste0(U, prime_suffix),
-                                               Y_prime = paste0(Y, prime_suffix)) {
+                                               R_colname = IEATools::psut_cols$R, 
+                                               U_colname = IEATools::psut_cols$U,
+                                               U_feed_colname = IEATools::psut_cols$U_feed,
+                                               U_eiou_colname = IEATools::psut_cols$U_eiou,
+                                               r_eiou_colname = IEATools::psut_cols$r_eiou,
+                                               V_colname = IEATools::psut_cols$V,
+                                               Y_colname = IEATools::psut_cols$Y, 
+                                               prime_suffix = "_prime") {
   
-  reallocate_func <- function(R_mat, U_feed_mat, U_eiou_mat, V_mat, Y_mat) {
-    # Reallocate Stat diffs from the R matrix to the V matrix.
-    R_stat_diffs <- R_mat |> 
-      matsbyname::select_rows_byname(retain_pattern = stat_diffs, fixed = TRUE)
+  R_prime = paste0(R_colname, prime_suffix)
+  U_prime = paste0(U_colname, prime_suffix)
+  U_feed_prime = paste0(U_feed_colname, prime_suffix)
+  U_eiou_prime = paste0(U_eiou_colname, prime_suffix)
+  r_eiou_prime = paste0(r_eiou_colname, prime_suffix)
+  V_prime = paste0(V_colname, prime_suffix)
+  Y_prime = paste0(Y_colname, prime_suffix)
+  
+  reallocate_func <- function(R_mat, U_mat, U_feed_mat, U_eiou_mat, r_eiou_mat, V_mat, Y_mat) {
+
+    # Store rownames of R and V
+    rownames_R_mat <- rownames(R_mat)
+    rownames_V_mat <- rownames(V_mat)
+    # Store colnames of U and Y
+    colnames_U_mat <- colnames(U_mat)
+    colnames_Y_mat <- colnames(Y_mat)
     
-    # If there is no Stat diffs row in R_mat, 
-    # R_stat_diffs will be NULL.
-    if (!is.null(R_stat_diffs)) {
-      V_mat_prime <- V_mat |> 
-        # Add to the V matrix
-        matsbyname::sum_byname(R_stat_diffs) |> 
-        # Reallocate in proportion to other producers
-        matsbyname::reallocate_byname(rowcolnames = stat_diffs, margin = 1)
-      
-      # Remove from the R matrix
-      R_mat_prime <- R_mat |> 
-        matsbyname::select_rows_byname(remove_pattern = stat_diffs, fixed = TRUE)
-    } else {
-      R_mat_prime <- R_mat
-      V_mat_prime <- V_mat
-    }
+    # Find (Y statdiffs rows with no other consumption in U or Y).
     
     
     
+    # Move (Y statdiffs rows with no other consumption in U or Y) to R by subtraction.
     
     
-    # Reallocate Stat diffs from the Y matrix to the U_feed matrix.
-    Y_stat_diffs <- Y_mat |> 
-      matsbyname::select_cols_byname(retain_pattern = stat_diffs, fixed = TRUE)  
     
-    # If there is no Stat diffs column in Y_mat, 
-    # Y_stat_diffs will be NULL.
-    if (!is.null(Y_stat_diffs)) {
-      U_feed_mat_prime <- U_feed_mat |> 
-        # Add to the U_feed matrix
-        matsbyname::sum_byname(Y_stat_diffs) |> 
-        # Reallocate in proportion to other consumers
-        matsbyname::reallocate_byname(rowcolnames = stat_diffs, margin = 2)
-      # Remove from Y matrix
-      Y_mat_prime <- Y_mat |> 
-        matsbyname::select_cols_byname(remove_pattern = stat_diffs, fixed = TRUE)
-    } else {
-      U_feed_mat_prime <- U_feed_mat
-      Y_mat_prime <- Y_mat
-    }
-    list(R_mat_prime, U_mat_prime, U_feed_mat_prime, V_mat_prime, Y_mat_prime) |> 
+    # Sum R and V.
+    # Reallocate negative statdiffs only into R+V.
+    # Split R and V using stored rownames.
+    
+    
+    # Move remaining R statdiffs to Y by subtraction.
+
+        
+    # Sum U and Y.
+    # Reallocate stadiffs columns into U+Y.
+    # Split U and Y using stored colnames.
+    
+    
+    # Calculate U_eiou = U * r_eiou (Hadamard product)
+    # Calculate U_feed = U - U_eiou
+    
+    
+    
+    
+    
+    
+    
+    # # Reallocate Stat diffs from the R matrix to the V matrix.
+    # R_stat_diffs <- R_mat |> 
+    #   matsbyname::select_rows_byname(retain_pattern = stat_diffs, fixed = TRUE)
+    # 
+    # # If there is no Stat diffs row in R_mat, 
+    # # R_stat_diffs will be NULL.
+    # if (!is.null(R_stat_diffs)) {
+    #   V_mat_prime <- V_mat |> 
+    #     # Add to the V matrix
+    #     matsbyname::sum_byname(R_stat_diffs) |> 
+    #     # Reallocate in proportion to other producers
+    #     matsbyname::reallocate_byname(rowcolnames = stat_diffs, margin = 1)
+    #   
+    #   # Remove from the R matrix
+    #   R_mat_prime <- R_mat |> 
+    #     matsbyname::select_rows_byname(remove_pattern = stat_diffs, fixed = TRUE)
+    # } else {
+    #   R_mat_prime <- R_mat
+    #   V_mat_prime <- V_mat
+    # }
+    # 
+    # 
+    # 
+    # 
+    # 
+    # # Reallocate Stat diffs from the Y matrix to the U_feed matrix.
+    # Y_stat_diffs <- Y_mat |> 
+    #   matsbyname::select_cols_byname(retain_pattern = stat_diffs, fixed = TRUE)  
+    # 
+    # # If there is no Stat diffs column in Y_mat, 
+    # # Y_stat_diffs will be NULL.
+    # if (!is.null(Y_stat_diffs)) {
+    #   U_feed_mat_prime <- U_feed_mat |> 
+    #     # Add to the U_feed matrix
+    #     matsbyname::sum_byname(Y_stat_diffs) |> 
+    #     # Reallocate in proportion to other consumers
+    #     matsbyname::reallocate_byname(rowcolnames = stat_diffs, margin = 2)
+    #   # Remove from Y matrix
+    #   Y_mat_prime <- Y_mat |> 
+    #     matsbyname::select_cols_byname(remove_pattern = stat_diffs, fixed = TRUE)
+    # } else {
+    #   U_feed_mat_prime <- U_feed_mat
+    #   Y_mat_prime <- Y_mat
+    # }
+    list(R_mat_prime, U_mat_prime, U_feed_mat_prime, U_eiou_mat_prime, 
+         V_mat_prime, Y_mat_prime) |> 
       magrittr::set_names(c(R_prime, U_prime, U_feed_prime, V_prime, Y_prime))
   }
   matsindf::matsindf_apply(.sutmats, FUN = reallocate_func, 
-                           R_mat = R, U_feed_mat = U_feed, U_eiou_mat = U_eiou, 
+                           R_mat = R, U_mat = U, U_feed_mat = U_feed, U_eiou_mat = U_eiou, 
+                           r_eiou_mat = r_eiou,
                            V_mat = V, Y_mat = Y)
 }
 
--- tests/testthat/test-reallocate.R
+++ tests/testthat/test-reallocate.R
@@ -1,9 +1,33 @@
 test_that("reallocate_statistical_differences() works as expected", { 
-  psut <- load_tidy_iea_df() |> 
-    specify_all() |>  
-    prep_psut() |> 
-    dplyr::slice_head(n = 1)
-  psut_reallocated <- psut |> 
-    reallocate_statistical_differences()
-  expect_true(!is.null(psut_reallocated))
+  
+  R <- matrix(c(98, 0, 
+                0, 50, 
+                2, 0), 
+              byrow = TRUE, nrow = 3, ncol = 2, 
+              dimnames = list(c("Resources [of Coal]", "Resources [of Prod C]", "Statistical differences"), 
+                              c("Coal [from Resources]", "Prod C")))
+  
+  U <- matrix(c(100, 
+                2), 
+              byrow = TRUE, nrow = 2, ncol = 1, 
+              dimnames = list(c("Coal [from Resources]", "Electricity"), 
+                              c("Mapep")))
+  
+  V <- matrix(40, 
+              byrow = TRUE, nrow = 1, ncol = 1, 
+              dimnames = list(c("Mapep"), c("Electricity")))
+  
+  Y <- matrix(c(20, 10, 8, 
+                0, 0, 50), 
+              byrow = TRUE, nrow = 2, ncol = 3, 
+              dimnames = list(c("Electricity", "Prod C"), 
+                              c("Industry 1", "Industry 2", "Statistical differences")))
+  
+  r_eiou <- matrix(1, 
+                   byrow = TRUE, nrow = 1, ncol = 1, 
+                   dimnames = list(c("Electricity"), 
+                                   c("Mapep")))
+  
+  U_eiou <- matsbyname::hadamardproduct_byname(U, r_eiou)
+  U_feed <- matsbyname::difference_byname(U, U_eiou)
 })
