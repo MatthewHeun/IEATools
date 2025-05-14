35848098a2b14a6bafc7eeb01013072d71814d60
Matthew Heun <matthew.heun@me.com>
2025-05-14 10:49:01 -0400

    Example for reallocate_statistical_differences()

--- R/reallocate.R
+++ R/reallocate.R
@@ -39,6 +39,52 @@
 #' @export
 #'
 #' @examples
+#' R <- matrix(c(98, 0, 
+#'                0, 50, 
+#'                2, 0), 
+#'             byrow = TRUE, nrow = 3, ncol = 2, 
+#'             dimnames = list(c("Resources [of Coal]", 
+#'                               "Resources [of Prod C]",
+#'                               "Statistical differences"), 
+#'                             c("Coal [from Resources]", "Prod C"))) |> 
+#'   matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product")
+#' R
+#' U <- matrix(c(100, 
+#'               2), 
+#'             byrow = TRUE, nrow = 2, ncol = 1, 
+#'             dimnames = list(c("Coal [from Resources]", "Electricity"), 
+#'                             c("Mapep"))) |> 
+#'   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry")
+#' U
+#' V <- matrix(40, 
+#'             byrow = TRUE, nrow = 1, ncol = 1, 
+#'             dimnames = list(c("Mapep"), c("Electricity"))) |> 
+#'   matsbyname::setrowtype("Industry") |> matsbyname::setcoltype("Product")
+#' V
+#' Y <- matrix(c(20, 10, 8, 
+#'                0, 0, 50), 
+#'             byrow = TRUE, nrow = 2, ncol = 3, 
+#'             dimnames = list(c("Electricity", "Prod C"), 
+#'                             c("Industry 1", "Industry 2", 
+#'                             "Statistical differences"))) |> 
+#'   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry")
+#' Y
+#' r_eiou <- matrix(1, 
+#'                  byrow = TRUE, nrow = 1, ncol = 1, 
+#'                  dimnames = list("Electricity", "Mapep")) |> 
+#'   matsbyname::setrowtype("Product") |> matsbyname::setcoltype("Industry")
+#' r_eiou
+#' U_EIOU <- matsbyname::hadamardproduct_byname(U, r_eiou)
+#' U_EIOU
+#' U_feed <- matsbyname::difference_byname(U, U_EIOU)
+#' U_feed
+#' reallocate_statistical_differences(R = R, 
+#'                                    U = U, 
+#'                                    U_feed = U_feed, 
+#'                                    U_eiou = U_EIOU, 
+#'                                    r_eiou = r_eiou, 
+#'                                    V = V,
+#'                                    Y = Y)
 reallocate_statistical_differences <- function(.sutmats = NULL, 
                                                stat_diffs = IEATools::tfc_compare_flows$statistical_differences, 
                                                R = IEATools::psut_cols$R, 
